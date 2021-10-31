d0f966735a6f5a279062ed6c27980bf192d0d251
Adrian Munoz <am00011@mix.wvu.edu>
2021-10-31 14:35:00 -0400

    added firebase library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,9 @@ import { HttpClientModule } from '@angular/common/http';
 import { UserInfoComponent } from './headers/user-info.component';
 import { EditUserInfoComponent } from './Body/edit-user-info.component';
 import { FormsModule } from '@angular/forms';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
 
 
 @NgModule({
@@ -28,7 +31,9 @@ import { FormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideDatabase(() => getDatabase())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'mlb-app-7942f',
+    appId: '1:1059593789360:web:4dd6d6da9a9e6f7d448075',
+    databaseURL: 'https://mlb-app-7942f-default-rtdb.firebaseio.com',
+    storageBucket: 'mlb-app-7942f.appspot.com',
+    apiKey: 'AIzaSyBC7WomF-vkPeU703CnQqJaM08Qm7qUiFM',
+    authDomain: 'mlb-app-7942f.firebaseapp.com',
+    messagingSenderId: '1059593789360',
+    measurementId: 'G-36HY4E1V9T',
+  },
   production: true
 };

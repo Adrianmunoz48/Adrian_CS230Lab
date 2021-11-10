cfdea42aaa3ed707b81c6699abb7f56ae99ba6b8
Adrian Munoz <am00011@mix.wvu.edu>
2021-11-09 22:04:05 -0500

    Added Authentication

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { BodyComponent } from './Body/body.component';
 import { RouterModule, Routes } from '@angular/router';
 import { TopBarComponent } from './headers/top-bar-component';
 import { footer } from './Footer/footer.component';
+import { AuthComponent } from './auth/auth.component';
 
 const routes: Routes = [
   {path: 'Home', component: BodyComponent },
   {path: 'search',component: footer},
-  {path: 'TopBar',component: TopBarComponent}
+  {path: 'TopBar',component: TopBarComponent},
+  {path: 'auth', component: AuthComponent},
+  
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import {TopBarComponent} from './headers/top-bar-component';
 import {footer} from './Footer/footer.component';
@@ -10,11 +9,12 @@ import { LayoutMainComponent } from './Layout/layout-main.component';
 import { HttpClientModule } from '@angular/common/http';
 import { UserInfoComponent } from './headers/user-info.component';
 import { EditUserInfoComponent } from './Body/edit-user-info.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideDatabase,getDatabase } from '@angular/fire/database';
 import { AngularFireModule } from '@angular/fire/compat';
+import { AuthComponent } from './auth/auth.component';
 
 
 
@@ -26,7 +26,9 @@ import { AngularFireModule } from '@angular/fire/compat';
     BodyComponent,
     LayoutMainComponent,
     UserInfoComponent,
-    EditUserInfoComponent
+    EditUserInfoComponent,
+    EditUserInfoComponent,
+    AuthComponent
 
   ],
   imports: [
@@ -34,6 +36,7 @@ import { AngularFireModule } from '@angular/fire/compat';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     AngularFireModule.initializeApp(environment.firebase, 'MLB-app'),
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideDatabase(() => getDatabase())
